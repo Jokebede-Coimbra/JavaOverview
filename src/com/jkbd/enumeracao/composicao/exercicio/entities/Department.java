36b01b4a42af46e115785b275de59392b8457829
Coimbra <adsjkbd@gmail.com>
2024-04-04 18:59:32 -0300

    Exercício resolvido 1 - Parte 1

---
+++ src/com/jkbd/enumeracao/composicao/exercicio/entities/Department.java
@@ -0,0 +1,21 @@
+package com.jkbd.enumeracao.composicao.exercicio.entities;
+
+public class Department {
+
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getNome() {
+        return name;
+    }
+
+    public void setNome(String nome) {
+        this.name = nome;
+    }
+}
---
+++ src/com/jkbd/enumeracao/composicao/exercicio/enums/WorkerLevel.java
@@ -0,0 +1,7 @@
+package com.jkbd.enumeracao.composicao.exercicio.enums;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR
+}
