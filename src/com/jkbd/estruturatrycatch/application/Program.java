a5256f29396df670fc15cceca375f5c7f2f1dbc3
Coimbra <adsjkbd@gmail.com>
2024-05-09 16:08:31 -0300

    Pilha de chamadas de métodos (stack trace)

---
+++ src/com/jkbd/estruturatrycatch/application/Program.java
@@ -0,0 +1,37 @@
+package com.jkbd.estruturatrycatch.application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        method1();
+        System.out.println("End of program");
+
+    }
+    public static void method1() {
+        System.out.println("***METHOD1 START***");
+        method2();
+        System.out.println("***METHOD1 END***");
+    }
+    public static void method2() {
+        System.out.println("***METHOD2 START***");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Entre com os nomes: ");
+            String[] vect = sc.nextLine().split(" ");
+            System.out.println("Digite uma posição: ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid position!");
+            e.printStackTrace();
+            sc.next();
+        } catch (InputMismatchException e) {
+            System.out.println("Input error");
+        }
+        sc.close();
+        System.out.println("***METHOD2 END***");
+    }
+}
