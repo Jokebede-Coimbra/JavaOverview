6014a39c1784d1cc862d830c9fa6f448b1cc9f22
Coimbra <adsjkbd@gmail.com>
2024-05-14 07:31:43 -0300

    Criando exceções personalizadas

---
+++ src/com/jkbd/tratamentoexcecoes/personalizadas/exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package com.jkbd.tratamentoexcecoes.personalizadas.exceptions;
+
+public class DomainException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DomainException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/com/jkbd/tratamentoexcecoes/personalizadas/model/entities/Reservation.java
@@ -0,0 +1,81 @@
+package com.jkbd.tratamentoexcecoes.personalizadas.model.entities;
+
+import com.jkbd.tratamentoexcecoes.personalizadas.exceptions.DomainException;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reservation {
+    private Integer rooNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public Reservation() {
+    }
+
+    public Reservation(Integer rooNumber, Date checkIn, Date checkOut) {
+        this.rooNumber = rooNumber;
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    public Integer getRooNumber() {
+        return rooNumber;
+    }
+
+    public void setRooNumber(Integer rooNumber) {
+        this.rooNumber = rooNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
+    public long duration() {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    }
+
+    public String updateDates(Date checkIn, Date checkOut) {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now)) {
+            throw new DomainException("Reservation dates for update must be future dates");
+        }
+        if (!checkOut.after(checkIn)) {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+        return null;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Room "
+                + rooNumber
+                + ", check-in: "
+                + sdf.format(checkIn)
+                + ", check-out: "
+                + sdf.format(checkOut)
+                + ",  "
+                + duration()
+                + " nights";
+
+    }
+}
