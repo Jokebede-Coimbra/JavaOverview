841b6e0df751808ae1b9f32c47ee5fc1e496ba17
Coimbra <adsjkbd@gmail.com>
2024-05-21 11:27:16 -0300

    exercício de fixação: tratamento de exceções

---
+++ src/com/jkbd/exercicios/exceptions/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package com.jkbd.exercicios.exceptions.exceptions;
+
+public class BusinessException extends RuntimeException {
+
+    public BusinessException(String msg) {
+        super(msg);
+    }
+}
