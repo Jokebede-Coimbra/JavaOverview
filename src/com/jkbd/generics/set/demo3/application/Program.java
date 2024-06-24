af6343beea49e0f957a40dee1aedf7643b4e16ee
Coimbra <adsjkbd@gmail.com>
2024-06-24 07:10:46 -0300

    TreeSet - comparando os elementos

---
+++ src/com/jkbd/generics/set/demo3/application/Program.java
@@ -0,0 +1,22 @@
+package com.jkbd.generics.set.demo3.application;
+
+import com.jkbd.generics.set.demo3.entities.Product;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Set<Product> set = new TreeSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        for (Product p : set) {
+            System.out.println(p);
+        }
+
+    }
+}
